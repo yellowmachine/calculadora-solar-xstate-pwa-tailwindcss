f4fb6a13fe029e523a9483866dc0c7ce8c1b04cc
Yellow Machine <yellow.machine@disroot.org>
2022-07-05 12:02:26 +0200

    wip

--- src/lib/machine.js
+++ src/lib/machine.js
@@ -87,7 +87,7 @@ export default createMachine(
                 target: 'radiationDone'
               },
               onError: {
-                actions: assign({error: (_, event) => "Error en invoke fetch radiation: " + JSON.stringify(event)})
+                actions: assign({error: (_, event) => "Error en invoke fetch radiation: " + JSON.stringify(event)}),
                 target: 'error'
               }
             }
